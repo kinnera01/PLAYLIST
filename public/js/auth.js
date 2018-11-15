a9c46476ba6eac9e3a215603317996ecc00d9395
KinneraVinay <kinnera.bezawada1@gmail.com>
2018-11-14 21:45:39 -0500

    trying to load youtube

--- public/js/auth.js
+++ public/js/auth.js
@@ -9,22 +9,17 @@
 //   core: "discogs_data_test",
 //   protocol: "http"
 // });
-var OAUTH2_CLIENT_ID ='144598218649-l78ovdbcde6bc30j11ac8bmjeqtbbuuj.apps.googleusercontent.com';
+var OAUTH2_CLIENT_ID = '144598218649-l78ovdbcde6bc30j11ac8bmjeqtbbuuj.apps.googleusercontent.com';
 var OAUTH2_SCOPES = [
   'https://www.googleapis.com/auth/youtube'
 ];
 
-// Upon loading, the Google APIs JS client automatically invokes this callback.
-googleApiClientReady = function() {
-  gapi.auth.init(function() {
+googleApiClientReady = function () {
+  gapi.auth.init(function () {
     window.setTimeout(checkAuth, 1);
   });
 }
-// Attempt the immediate OAuth 2.0 client flow as soon as the page loads.
-// If the currently logged-in Google Account has previously authorized
-// the client specified as the OAUTH2_CLIENT_ID, then the authorization
-// succeeds with no user intervention. Otherwise, it fails and the
-// user interface that prompts for authorization needs to display.
+
 function checkAuth() {
   gapi.auth.authorize({
     client_id: OAUTH2_CLIENT_ID,
@@ -32,60 +27,141 @@ function checkAuth() {
     immediate: true
   }, handleAuthResult);
 }
-
-
-// var youtubeids = [];
-// const express = require("express");
-// const app = express();
-// var strQuery = client
-//   .query()
-//   .q({ releaseDate: "2016" })
-//   .sort({ viewcountRate: "desc" })
-//   .start(0)
-//   .rows(20);
-//   console.log(strQuery);
-// client.search(strQuery, function(err, result) {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   }
-//   // console.log("Response:", result.response.docs);
-//   var docs = result.response.docs;
-//   docs.forEach(element => {
-//     youtubeids.push(element.youtubeId);
-//   });
-//   console.log(youtubeids);
-//   return youtubeids;
-// });
-
-// Handle the result of a gapi.auth.authorize() call.
 function handleAuthResult(authResult) {
   if (authResult && !authResult.error) {
-    // Authorization was successful. Hide authorization prompts and show
-    // content that should be visible after authorization succeeds.
+
     $('.pre-auth').hide();
     $('.post-auth').show();
     loadAPIClientInterfaces();
   } else {
-    // Make the #login-link clickable. Attempt a non-immediate OAuth 2.0
-    // client flow. The current function is called when that flow completes.
-    $('#login-link').click(function() {
+    
+    $('#login-link').click(function () {
       gapi.auth.authorize({
         client_id: OAUTH2_CLIENT_ID,
         scope: OAUTH2_SCOPES,
         immediate: false
-        }, handleAuthResult);
+      }, handleAuthResult);
     });
   }
 }
 
-// Load the client interfaces for the YouTube Analytics and Data APIs, which
-// are required to use the Google APIs JS client. More info is available at
-// https://developers.google.com/api-client-library/javascript/dev/dev_jscript#loading-the-client-library-and-the-api
 function loadAPIClientInterfaces() {
-  gapi.client.load('youtube', 'v3', function() {
+  gapi.client.load('youtube', 'v3', function () {
     handleAPILoaded();
   });
 }
-// http://aurora.cs.rutgers.edu:8181/solr/#/discogs_data_test
-// http://aurora.cs.rutgers.edu:8181/solr/discogs_data_test/select?q=*%3A*&wt=json&indent=true
+function handleAPILoaded() {
+  enableForm();
+}
+// Enable the form for creating a playlist.
+function enableForm() {
+  $("#playlist-button").attr("disabled", false);
+}
+
+// // Create a private playlist.
+// function createPlaylist() {
+//   var title = $("#Title").val();
+//   var description = $("#Desciption").val();
+//   console.log(title);
+//   console.log(description);
+//   var request = gapi.client.youtube.playlists.insert({
+//     part: "snippet,status",
+//     resource: {
+//       snippet: {
+//         title: title,
+//         description: description
+//       },
+//       status: {
+//         privacyStatus: "public"
+//       }
+//     }
+//   });
+//   request.execute(function(response) {
+//     var result = response.result;
+//     console.log(result);
+//     if (result) {
+//       playlistId = result.id;
+//       // console.log(playlistId)
+//       $("#playlist-id").val(playlistId);
+//       $("#playlist-Id").html(result.id);
+//     } else {
+//       $("#status").html("Could not create playlist");
+//     }
+//   });
+// }
+
+// // var videoId=["ZG1Su0QwPYs","_JVghQCWnRI","Y-xZIECiTwk"]
+// // Add a video ID specified in the form to the playlist.
+// var addVideoToPlaylist=function() {
+//   getyoutubeids();
+//   //addToPlaylist($("#video-id").val());
+// }
+// function getyoutubeids(){
+//   console.log("HEY I AM IN YIDS")
+//     // $("#year").empty();
+//     var year=$("#year").val();
+//     // console.log(year);
+//     //aurora.cs.rutgers.edu:8181/solr/discogs_data_test/select?q=releaseDate:"+year+'&sort=viewcountRate%20desc&start=0&rows=50&wt=json&indent=true
+//     var queryurl="//aurora.cs.rutgers.edu:8181/solr/#/discogs_data_test/query/select?q=releaseDate:"+year+"&sort=viewcountRate=desc&start=0&rows=50&wt=json&indent=true";
+//     jQuery.support.cors = true;
+//       $.getJSON(queryurl).done(function(res){
+//       //   console.log("HEY")
+//       // console.log("i am response"+res.response.docs)
+//       var docs = res.response.docs;
+//       console.log(docs)
+//         docs.forEach(element => {
+//           // youtubeids.push(element.youtubeId);
+//           console.log(element.youtubeId);
+//           addToPlaylist(element.youtubeId);
+//         });
+//       });
+// // Add a video to a playlist. The "startPos" and "endPos" values let you
+// // start and stop the video at specific times when the video is played as
+// // part of the playlist. However, these values are not set in this example.
+// function addToPlaylist(id, startPos, endPos) {
+//   var playid = $("#playlist-id").val();
+//   console.log(
+//     "in addToPlaylist with " + id + "sending to playlist : " + playid
+//   );
+//   var details = {
+//     playlistId: playid,
+//     videoId: id,
+//     kind: "youtube#video"
+//   };
+//   console.log(details);
+//   if (startPos != undefined) {
+//     details["startAt"] = startPos;
+//   }
+//   if (endPos != undefined) {
+//     details["endAt"] = endPos;
+//   }
+//   // console.log(playlistId);
+//   var request = gapi.client.youtube.playlistItems.insert({
+//     part: "snippet",
+//     resource: {
+//       snippet: {
+//         playlistId: details.playlistId,
+//         resourceId: details
+//       }
+//     }
+//   });
+//   //console.log(request)
+//   request.execute(function(response) {
+//     $("#status").html("<pre>" + JSON.stringify(response.result) + "</pre>");
+//   });
+// }
+// function addTheseVideosToPlaylist() {
+//   var links = youtubeids;
+//   var counter = 0;
+//   function addVideosToPlaylist() {
+//     myLoop(links[0]);
+//   }
+//   function myLoop(video_id) {
+//     addToPlaylist(video_id);
+//     setTimeout(function() {
+//       counter++;
+//       if (counter < links.length) myLoop(links[counter]);
+//     }, 3000);
+//   }
+// }
+// }
