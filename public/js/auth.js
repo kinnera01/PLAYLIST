ff4233e422534dacf622b8fbd49a4a838d5cefab
KinneraVinay <kinnera.bezawada1@gmail.com>
2018-11-14 22:53:11 -0500

    trying to load youtube

--- public/js/auth.js
+++ public/js/auth.js
@@ -13,6 +13,7 @@ var OAUTH2_CLIENT_ID = '144598218649-cs7ktbbd4q7ghanjvbluj51d5vt9djok.apps.googl
 var OAUTH2_SCOPES = [
   'https://www.googleapis.com/auth/youtube'
 ];
+var videoids;
 
 googleApiClientReady = function () {
   gapi.auth.init(function () {
@@ -93,122 +94,70 @@ $("#playlist-button").on("click", function () {
   });
 
 })
-$("#Addvideo").on("click", function() {
+$("#Addvideo").on("click", function () {
   var result = {
     id: $("#playlist-id").val(),
     year: $("#year").val(),
     videoid: $("#video-id").val()
   }
   console.log(result);
-  $.post("/songs", result, function(data) {
+  $.post("/songs", result, function (data) {
     // Grab the result from the AJAX post so that the best match's name and photo are displayed.
     console.log("data posted to routes");
+    console.log("incoming data", data)
+    videoids = data;
+    videoids.forEach(element => {
+      // youtubeids.push(element.youtubeId);
+      console.log(element);
+      addToPlaylist(element);
+    });
   })
+ 
+  function addToPlaylist(id, startPos, endPos) {
+    
+    var playid = $("#playlist-id").val();
+    console.log(
+      "in addToPlaylist with " + id + "sending to playlist : " + playid
+    );
+    var details = {
+      playlistId: playid,
+      videoId: id,
+      kind: "youtube#video"
+    };
+    console.log(details);
+    if (startPos != undefined) {
+      details["startAt"] = startPos;
+    }
+    if (endPos != undefined) {
+      details["endAt"] = endPos;
+    }
+    // console.log(playlistId);
+    var request = gapi.client.youtube.playlistItems.insert({
+      part: "snippet",
+      resource: {
+        snippet: {
+          playlistId: details.playlistId,
+          resourceId: details
+        }
+      }
+    });
+    //console.log(request)
+    request.execute(function(response) {
+      $("#status").html("<pre>" + JSON.stringify(response.result) + "</pre>");
+    });
+  }
+  function addTheseVideosToPlaylist() {
+    var links = youtubeids;
+    var counter = 0;
+    function addVideosToPlaylist() {
+      myLoop(links[0]);
+    }
+    function myLoop(video_id) {
+      addToPlaylist(video_id);
+      setTimeout(function() {
+        counter++;
+        if (counter < links.length) myLoop(links[counter]);
+      }, 3000);
+    }
+  }
 })
-// // Create a private playlist.
-// function createPlaylist() {
-//   var title = $("#Title").val();
-//   var description = $("#Desciption").val();
-//   console.log(title);
-//   console.log(description);
-//   var request = gapi.client.youtube.playlists.insert({
-//     part: "snippet,status",
-//     resource: {
-//       snippet: {
-//         title: title,
-//         description: description
-//       },
-//       status: {
-//         privacyStatus: "public"
-//       }
-//     }
-//   });
-//   request.execute(function(response) {
-//     var result = response.result;
-//     console.log(result);
-//     if (result) {
-//       playlistId = result.id;
-//       // console.log(playlistId)
-//       $("#playlist-id").val(playlistId);
-//       $("#playlist-Id").html(result.id);
-//     } else {
-//       $("#status").html("Could not create playlist");
-//     }
-//   });
-// }
-
-// // var videoId=["ZG1Su0QwPYs","_JVghQCWnRI","Y-xZIECiTwk"]
-// // Add a video ID specified in the form to the playlist.
-// var addVideoToPlaylist=function() {
-//   getyoutubeids();
-//   //addToPlaylist($("#video-id").val());
-// }
-// function getyoutubeids(){
-//   console.log("HEY I AM IN YIDS")
-//     // $("#year").empty();
-//     var year=$("#year").val();
-//     // console.log(year);
-//     //aurora.cs.rutgers.edu:8181/solr/discogs_data_test/select?q=releaseDate:"+year+'&sort=viewcountRate%20desc&start=0&rows=50&wt=json&indent=true
-//     var queryurl="//aurora.cs.rutgers.edu:8181/solr/#/discogs_data_test/query/select?q=releaseDate:"+year+"&sort=viewcountRate=desc&start=0&rows=50&wt=json&indent=true";
-//     jQuery.support.cors = true;
-//       $.getJSON(queryurl).done(function(res){
-//       //   console.log("HEY")
-//       // console.log("i am response"+res.response.docs)
-//       var docs = res.response.docs;
-//       console.log(docs)
-//         docs.forEach(element => {
-//           // youtubeids.push(element.youtubeId);
-//           console.log(element.youtubeId);
-//           addToPlaylist(element.youtubeId);
-//         });
-//       });
-// // Add a video to a playlist. The "startPos" and "endPos" values let you
-// // start and stop the video at specific times when the video is played as
-// // part of the playlist. However, these values are not set in this example.
-// function addToPlaylist(id, startPos, endPos) {
-//   var playid = $("#playlist-id").val();
-//   console.log(
-//     "in addToPlaylist with " + id + "sending to playlist : " + playid
-//   );
-//   var details = {
-//     playlistId: playid,
-//     videoId: id,
-//     kind: "youtube#video"
-//   };
-//   console.log(details);
-//   if (startPos != undefined) {
-//     details["startAt"] = startPos;
-//   }
-//   if (endPos != undefined) {
-//     details["endAt"] = endPos;
-//   }
-//   // console.log(playlistId);
-//   var request = gapi.client.youtube.playlistItems.insert({
-//     part: "snippet",
-//     resource: {
-//       snippet: {
-//         playlistId: details.playlistId,
-//         resourceId: details
-//       }
-//     }
-//   });
-//   //console.log(request)
-//   request.execute(function(response) {
-//     $("#status").html("<pre>" + JSON.stringify(response.result) + "</pre>");
-//   });
-// }
-// function addTheseVideosToPlaylist() {
-//   var links = youtubeids;
-//   var counter = 0;
-//   function addVideosToPlaylist() {
-//     myLoop(links[0]);
-//   }
-//   function myLoop(video_id) {
-//     addToPlaylist(video_id);
-//     setTimeout(function() {
-//       counter++;
-//       if (counter < links.length) myLoop(links[counter]);
-//     }, 3000);
-//   }
-// }
-// }
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,47 +3,40 @@ var SolrNode = require('solr-node');
  
 // Create client
 var client = new SolrNode({
-    host: '127.0.0.1',
-    port: '8983',
-    core: 'test',
-    protocol: 'http'
+  host: "aurora.cs.rutgers.edu",
+    port: "8181",
+    core: "discogs_data_test",
+    protocol: "http"
 });
- 
-var info;
+var youtubeids = [];
+//ar info;
 module.exports = function(app) {
   // Get all examples
    app.get("/", function(req, res) {
     res.render("index");
   });
 app.post("/songs",function(req,res){
-  console.log(req.body);
-  info=req.body;
-  
+  console.log(req.body)
+  var strQuery = client
+  .query()
+  .q({ releaseDate: req.body.year })
+  .sort({ viewcountRate: "desc" })
+  .start(0)
+  .rows(20);
+  // console.log(strQuery);
+client.search(strQuery, function(err, result) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  // console.log("Response:", result.response.docs);
+  var docs = result.response.docs;
+  docs.forEach(element => {
+    youtubeids.push(element.youtubeId);
+  });
+   console.log(youtubeids);
+   res.send(youtubeids);
+});
 })
-// app.get('/getProduct',
-//  function (req, res) 
-//  { 
-// var strQuery = client.query().q('productId:9788700075740');
-//  client.search(strQuery, function (err, result) {
-//    if (err) {
-//       console.log(err);
-//       return;
-//    }
-//    console.log('Response:', result.response);
-//    res.send(result.response);
-// });
-//  }); 
-  // // Create a new example
-  // app.post("/api/examples", function(req, res) {
-  //   db.Example.create(req.body).then(function(dbExample) {
-  //     res.json(dbExample);
-  //   });
-  // });
 
-  // // Delete an example by id
-  // app.delete("/api/examples/:id", function(req, res) {
-  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.json(dbExample);
-  //   });
-  //});
 };
